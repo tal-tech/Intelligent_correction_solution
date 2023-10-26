07233d1c5ee7d306abdab63e052d2f5b185d37e0
mayang_tal <mayang8@tal.com>
2023-10-26 20:32:06 +0800

    first commit

---
+++ correction_symbols/pushCode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# push code
+
+rm -rf .git/
+echo "请输入git地址:";
+read git_ku
+git init
+git add ./*
+git commit -m "first commit"
+git remote add origin1 ${git_ku}
+git push origin1 master
+rm -rf .git/
